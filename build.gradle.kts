a35bf83a01c2fab31425fe1b39431a9a571f5ae1
syorito-hatsuki <enderdron3@gmail.com>
2023-12-26 21:38:48 +0200

    Removed LICENSE file renaming

--- build.gradle.kts
+++ build.gradle.kts
@@ -63,11 +63,7 @@ tasks {
     }
 
     jar {
-        from("LICENSE") {
-            rename {
-                "${it}_${base.archivesName}"
-            }
-        }
+        from("LICENSE")
     }
 
     processResources {
