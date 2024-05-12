6e4f862ffef9681ee2a44b15ce5dbe7ca035e24b
syorito-hatsuki <enderdron3@gmail.com>
2024-05-12 13:42:09 +0300

    Moved Kotlin code to kotlin package

--- src/main/kotlin/com/technicjelle/bluemapofflineplayermarkers/ConfigManager.kt
+++ src/main/kotlin/com/technicjelle/bluemapofflineplayermarkers/ConfigManager.kt
@@ -1,7 +1,6 @@
 package com.technicjelle.bluemapofflineplayermarkers
 
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import java.io.File
